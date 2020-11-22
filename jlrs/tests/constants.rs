72d959b45a434ea79ad6cac1799b11336558a9a3
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2020-11-22 19:44:42 +0100

    Tests for UnionAll and DataType constants

--- jlrs/tests/constants.rs
+++ jlrs/tests/constants.rs
@@ -1,5 +1,6 @@
 use jlrs::prelude::*;
 use jlrs::util::JULIA;
+use jlrs::value::union_all::UnionAll;
 
 macro_rules! impl_constant_test {
     ($func:ident, $tyname:expr) => {
@@ -64,6 +65,63 @@ macro_rules! impl_constant_subtype_test {
     };
 }
 
+macro_rules! impl_unionall_constant_test {
+    ($func:ident, $tyname:expr) => {
+        #[test]
+        fn $func() {
+            JULIA.with(|j| {
+                let mut jlrs = j.borrow_mut();
+
+                jlrs.frame(0, |global, _| {
+                    let v1 = UnionAll::$func(global);
+                    let v2 = Module::core(global).global($tyname)?;
+                    assert!(v1.as_value().egal(v2));
+                    Ok(())
+                })
+                .unwrap();
+            });
+        }
+    };
+}
+
+macro_rules! impl_unionall_constant_isa_test {
+    ($func:ident, $tyname:expr) => {
+        #[test]
+        fn $func() {
+            JULIA.with(|j| {
+                let mut jlrs = j.borrow_mut();
+
+                jlrs.frame(0, |global, _| {
+                    let v1 = UnionAll::$func(global);
+                    let v2 = Module::core(global).global($tyname)?;
+                    assert!(v1.as_value().isa(v2));
+                    Ok(())
+                })
+                .unwrap();
+            });
+        }
+    };
+}
+
+macro_rules! impl_datatype_constant_isa_test {
+    ($func:ident, $tyname:expr) => {
+        #[test]
+        fn $func() {
+            JULIA.with(|j| {
+                let mut jlrs = j.borrow_mut();
+
+                jlrs.frame(0, |global, _| {
+                    let v1 = DataType::$func(global);
+                    let v2 = Module::core(global).global($tyname)?;
+                    assert!(v1.as_value().isa(v2));
+                    Ok(())
+                })
+                .unwrap();
+            });
+        }
+    };
+}
+
 impl_constant_test!(bottom_type, "TypeofBottom");
 impl_constant_test!(stackovf_exception, "StackOverflowError");
 impl_constant_test!(memory_exception, "OutOfMemoryError");
@@ -82,344 +140,83 @@ impl_constant_isa_test!(true_v, "Bool");
 impl_constant_isa_test!(false_v, "Bool");
 impl_constant_isa_test!(nothing, "Nothing");
 
-/*
-
-    /// The instance of `true`.
-    pub fn true_v(_: Global<'base>) -> Self {
-        unsafe { Value::wrap(jl_true) }
-    }
-
-    /// The instance of `false`.
-    pub fn false_v(_: Global<'base>) -> Self {
-        unsafe { Value::wrap(jl_false) }
-    }
-
-    /// The instance of `Core.Nothing`, `nothing`.
-    pub fn nothing(_: Global<'base>) -> Self {
-        unsafe { Value::wrap(jl_nothing) }
-    }
-*/
-
-/*
-impl<'base> UnionAll<'base> {
-    pub fn type_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_type_type) }
-    }
-
-    pub fn typetype_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_typetype_type) }
-    }
-
-    pub fn anytuple_type_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_anytuple_type_type) }
-    }
-
-    pub fn vararg_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_vararg_type) }
-    }
-
-    pub fn abstractarray_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_abstractarray_type) }
-    }
-
-    pub fn densearray_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_densearray_type) }
-    }
-
-    pub fn array_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_array_type) }
-    }
-
-    pub fn pointer_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_pointer_type) }
-    }
-
-    pub fn llvmpointer_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_llvmpointer_type) }
-    }
-
-    pub fn ref_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_ref_type) }
-    }
-
-    pub fn namedtuple_type(_: Global<'base>) -> Self {
-        unsafe { UnionAll::wrap(jl_namedtuple_type) }
-    }
-}
-*/
-
-/*
-impl<'base> DataType<'base> {
-    pub fn typeofbottom_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_typeofbottom_type) }
-    }
-
-    pub fn datatype_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_datatype_type) }
-    }
-
-    pub fn uniontype_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_uniontype_type) }
-    }
-
-    pub fn unionall_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_unionall_type) }
-    }
-
-    pub fn tvar_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_tvar_type) }
-    }
-
-    pub fn any_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_any_type) }
-    }
-
-    pub fn typename_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_typename_type) }
-    }
-
-    pub fn symbol_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_symbol_type) }
-    }
-
-    pub fn ssavalue_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_ssavalue_type) }
-    }
-
-    pub fn abstractslot_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_abstractslot_type) }
-    }
-
-    pub fn slotnumber_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_slotnumber_type) }
-    }
-
-    pub fn typedslot_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_typedslot_type) }
-    }
-
-    pub fn simplevector_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_simplevector_type) }
-    }
-
-    pub fn anytuple_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_anytuple_type) }
-    }
-
-    pub fn tuple_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_anytuple_type) }
-    }
-
-    pub fn emptytuple_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_emptytuple_type) }
-    }
-
-    pub fn function_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_function_type) }
-    }
-
-    pub fn builtin_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_builtin_type) }
-    }
-
-    pub fn method_instance_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_method_instance_type) }
-    }
-
-    pub fn code_instance_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_code_instance_type) }
-    }
-
-    pub fn code_info_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_code_info_type) }
-    }
-
-    pub fn method_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_method_type) }
-    }
-
-    pub fn module_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_module_type) }
-    }
-
-    pub fn weakref_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_weakref_type) }
-    }
-
-    pub fn abstractstring_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_abstractstring_type) }
-    }
-
-    pub fn string_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_string_type) }
-    }
-
-    pub fn errorexception_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_errorexception_type) }
-    }
-
-    pub fn argumenterror_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_argumenterror_type) }
-    }
-
-    pub fn loaderror_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_loaderror_type) }
-    }
-
-    pub fn initerror_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_initerror_type) }
-    }
-
-    pub fn typeerror_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_typeerror_type) }
-    }
-
-    pub fn methoderror_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_methoderror_type) }
-    }
-
-    pub fn undefvarerror_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_undefvarerror_type) }
-    }
-
-    pub fn lineinfonode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_lineinfonode_type) }
-    }
-
-    pub fn boundserror_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_boundserror_type) }
-    }
-
-    pub fn bool_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_bool_type) }
-    }
-
-    pub fn char_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_char_type) }
-    }
-
-    pub fn int8_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_int8_type) }
-    }
-
-    pub fn uint8_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_uint8_type) }
-    }
-
-    pub fn int16_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_int16_type) }
-    }
-
-    pub fn uint16_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_uint16_type) }
-    }
-
-    pub fn int32_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_int32_type) }
-    }
-
-    pub fn uint32_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_uint32_type) }
-    }
-
-    pub fn int64_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_int64_type) }
-    }
-
-    pub fn uint64_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_uint64_type) }
-    }
-
-    pub fn float16_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_float16_type) }
-    }
-
-    pub fn float32_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_float32_type) }
-    }
-
-    pub fn float64_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_float64_type) }
-    }
-
-    pub fn floatingpoint_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_floatingpoint_type) }
-    }
-
-    pub fn number_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_number_type) }
-    }
-
-    pub fn nothing_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_nothing_type) }
-    }
-
-    pub fn signed_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_signed_type) }
-    }
-
-    pub fn voidpointer_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_voidpointer_type) }
-    }
-
-    pub fn task_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_task_type) }
-    }
-
-    pub fn expr_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_expr_type) }
-    }
-
-    pub fn globalref_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_globalref_type) }
-    }
-
-    pub fn linenumbernode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_linenumbernode_type) }
-    }
-
-    pub fn gotonode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_gotonode_type) }
-    }
-
-    pub fn phinode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_phinode_type) }
-    }
-
-    pub fn pinode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_pinode_type) }
-    }
-
-    pub fn phicnode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_phicnode_type) }
-    }
-
-    pub fn upsilonnode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_upsilonnode_type) }
-    }
-
-    pub fn quotenode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_quotenode_type) }
-    }
-
-    pub fn newvarnode_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_newvarnode_type) }
-    }
-
-    pub fn intrinsic_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_intrinsic_type) }
-    }
-
-    pub fn methtable_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_methtable_type) }
-    }
-
-    pub fn typemap_level_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_typemap_level_type) }
-    }
-
-    pub fn typemap_entry_type(_: Global<'base>) -> Self {
-        unsafe { Self::wrap(jl_typemap_entry_type) }
-    }
-}
-*/
+impl_unionall_constant_test!(type_type, "Type");
+impl_unionall_constant_isa_test!(typetype_type, "Type");
+impl_unionall_constant_isa_test!(anytuple_type_type, "Type");
+impl_unionall_constant_test!(vararg_type, "Vararg");
+impl_unionall_constant_isa_test!(abstractarray_type, "Type");
+impl_unionall_constant_isa_test!(densearray_type, "Type");
+impl_unionall_constant_isa_test!(array_type, "Type");
+impl_unionall_constant_isa_test!(pointer_type, "Type");
+impl_unionall_constant_isa_test!(llvmpointer_type, "Type");
+impl_unionall_constant_isa_test!(ref_type, "Type");
+impl_unionall_constant_isa_test!(namedtuple_type, "Type");
+
+impl_datatype_constant_isa_test!(typeofbottom_type, "DataType");
+impl_datatype_constant_isa_test!(datatype_type, "DataType");
+impl_datatype_constant_isa_test!(uniontype_type, "DataType");
+impl_datatype_constant_isa_test!(unionall_type, "DataType");
+impl_datatype_constant_isa_test!(tvar_type, "DataType");
+impl_datatype_constant_isa_test!(any_type, "DataType");
+impl_datatype_constant_isa_test!(typename_type, "DataType");
+impl_datatype_constant_isa_test!(symbol_type, "DataType");
+impl_datatype_constant_isa_test!(ssavalue_type, "DataType");
+impl_datatype_constant_isa_test!(abstractslot_type, "DataType");
+impl_datatype_constant_isa_test!(slotnumber_type, "DataType");
+impl_datatype_constant_isa_test!(typedslot_type, "DataType");
+impl_datatype_constant_isa_test!(simplevector_type, "DataType");
+impl_datatype_constant_isa_test!(anytuple_type, "DataType");
+impl_datatype_constant_isa_test!(tuple_type, "DataType");
+impl_datatype_constant_isa_test!(emptytuple_type, "DataType");
+impl_datatype_constant_isa_test!(function_type, "DataType");
+impl_datatype_constant_isa_test!(builtin_type, "DataType");
+impl_datatype_constant_isa_test!(method_instance_type, "DataType");
+impl_datatype_constant_isa_test!(code_instance_type, "DataType");
+impl_datatype_constant_isa_test!(code_info_type, "DataType");
+impl_datatype_constant_isa_test!(method_type, "DataType");
+impl_datatype_constant_isa_test!(module_type, "DataType");
+impl_datatype_constant_isa_test!(weakref_type, "DataType");
+impl_datatype_constant_isa_test!(abstractstring_type, "DataType");
+impl_datatype_constant_isa_test!(string_type, "DataType");
+impl_datatype_constant_isa_test!(errorexception_type, "DataType");
+impl_datatype_constant_isa_test!(argumenterror_type, "DataType");
+impl_datatype_constant_isa_test!(loaderror_type, "DataType");
+impl_datatype_constant_isa_test!(initerror_type, "DataType");
+impl_datatype_constant_isa_test!(typeerror_type, "DataType");
+impl_datatype_constant_isa_test!(methoderror_type, "DataType");
+impl_datatype_constant_isa_test!(undefvarerror_type, "DataType");
+impl_datatype_constant_isa_test!(lineinfonode_type, "DataType");
+impl_datatype_constant_isa_test!(boundserror_type, "DataType");
+impl_datatype_constant_isa_test!(bool_type, "DataType");
+impl_datatype_constant_isa_test!(char_type, "DataType");
+impl_datatype_constant_isa_test!(int8_type, "DataType");
+impl_datatype_constant_isa_test!(uint8_type, "DataType");
+impl_datatype_constant_isa_test!(int16_type, "DataType");
+impl_datatype_constant_isa_test!(uint16_type, "DataType");
+impl_datatype_constant_isa_test!(int32_type, "DataType");
+impl_datatype_constant_isa_test!(uint32_type, "DataType");
+impl_datatype_constant_isa_test!(int64_type, "DataType");
+impl_datatype_constant_isa_test!(uint64_type, "DataType");
+impl_datatype_constant_isa_test!(float16_type, "DataType");
+impl_datatype_constant_isa_test!(float32_type, "DataType");
+impl_datatype_constant_isa_test!(float64_type, "DataType");
+impl_datatype_constant_isa_test!(floatingpoint_type, "DataType");
+impl_datatype_constant_isa_test!(number_type, "DataType");
+impl_datatype_constant_isa_test!(nothing_type, "DataType");
+impl_datatype_constant_isa_test!(signed_type, "DataType");
+impl_datatype_constant_isa_test!(voidpointer_type, "DataType");
+impl_datatype_constant_isa_test!(task_type, "DataType");
+impl_datatype_constant_isa_test!(expr_type, "DataType");
+impl_datatype_constant_isa_test!(globalref_type, "DataType");
+impl_datatype_constant_isa_test!(linenumbernode_type, "DataType");
+impl_datatype_constant_isa_test!(gotonode_type, "DataType");
+impl_datatype_constant_isa_test!(phinode_type, "DataType");
+impl_datatype_constant_isa_test!(pinode_type, "DataType");
+impl_datatype_constant_isa_test!(phicnode_type, "DataType");
+impl_datatype_constant_isa_test!(upsilonnode_type, "DataType");
+impl_datatype_constant_isa_test!(quotenode_type, "DataType");
+impl_datatype_constant_isa_test!(newvarnode_type, "DataType");
+impl_datatype_constant_isa_test!(intrinsic_type, "DataType");
+impl_datatype_constant_isa_test!(methtable_type, "DataType");
+impl_datatype_constant_isa_test!(typemap_level_type, "DataType");
+impl_datatype_constant_isa_test!(typemap_entry_type, "DataType");
