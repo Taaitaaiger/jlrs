80c1570527ee0b6503a5fcc1da23d4766735bafc
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2022-04-10 22:11:18 +0200

    Update coverage script

--- coverage.sh
+++ coverage.sh
@@ -4,13 +4,26 @@ set -euxo pipefail
 
 cargo clean
 
-LLVM_PROFILE_FILE="jlrs-%p-%m.profraw" \
-RUSTFLAGS="-Zinstrument-coverage" \
-cargo +nightly test --features sync-rt,tokio-rt,jlrs-derive,f16,jlrs-ndarray,ccall,uv \
---profile=coverage -- --test-threads=1
+LLVM_PROFILE_FILE="jlrs-%m-%p.profraw" \
+RUSTFLAGS="-C instrument-coverage" \
+cargo test --features sync-rt,tokio-rt,jlrs-derive,f16,jlrs-ndarray,ccall,uv --tests -- --test-threads=1
 
-grcov . -s . --binary-path ./target/coverage/ -t html --branch --ignore-not-existing -o \
-./target/coverage/coverage/ --ignore "jl_sys/*" --ignore "jlrs/tests/*" --ignore \
-"jlrs_derive_tests/*" --ignore "jlrs_async_tests/*" --ignore "jlrs/src/wrappers/ptr/internal/*"
+rust-profdata merge *.profraw -o jlrs.profdata
+
+rust-cov report \
+    $( \
+      for file in \
+        $( \
+          RUSTFLAGS="-C instrument-coverage" \
+            cargo test  --features sync-rt,tokio-rt,jlrs-derive,f16,jlrs-ndarray,ccall,uv --tests --no-run --message-format=json -- --test-threads=1 \
+              | jq -r "select(.profile.test == true) | .filenames[]" \
+              | grep -v dSYM - \
+        ); \
+      do \
+        printf "%s %s " -object $file; \
+      done \
+    ) \
+  --instr-profile=jlrs.profdata --summary-only
 
 find . -name "*.profraw" -print0 | xargs -0 rm
+rm ./*.profdata
