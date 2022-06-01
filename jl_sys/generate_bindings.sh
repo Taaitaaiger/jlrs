22bd72c419438aae259cfaa0df2800049be340ec
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2022-06-01 20:37:45 +0200

    Remove tests from generated bindings

--- jl_sys/generate_bindings.sh
+++ jl_sys/generate_bindings.sh
@@ -44,25 +44,21 @@ fi
 cargo clean
 JULIA_DIR=$JULIA_LTS_DIR_WIN LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$JULIA_DIR/lib:$JULIA_DIR/lib/julia" cargo build --features use-bindgen,windows-lts --target x86_64-pc-windows-gnu
 updated=$(sed -e '1h;2,$H;$!d;g' -r -E -e 's/(extern "C" \{\n\s+pub static)/#[link(name = \"libjulia\")]\n\1/g' ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs)
-echo "#![allow(deref_nullptr)]" > ./src/bindings_1_6_x86_64_pc_windows_gnu.rs
-echo "/* generated from Julia version 1.6.6 */" >> ./src/bindings_1_6_x86_64_pc_windows_gnu.rs
+echo "/* generated from Julia version 1.6.6 */" > ./src/bindings_1_6_x86_64_pc_windows_gnu.rs
 echo -e "$updated" >> ./src/bindings_1_6_x86_64_pc_windows_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_LTS_DIR LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$JULIA_DIR/lib:$JULIA_DIR/lib/julia" cargo build --features use-bindgen,lts
-echo "#![allow(deref_nullptr)]" > ./src/bindings_1_6_x86_64_unknown_linux_gnu.rs
-echo "/* generated from Julia version 1.6.6 */" >> ./src/bindings_1_6_x86_64_unknown_linux_gnu.rs
+echo "/* generated from Julia version 1.6.6 */" > ./src/bindings_1_6_x86_64_unknown_linux_gnu.rs
 cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_6_x86_64_unknown_linux_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_STABLE_DIR_WIN LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$JULIA_DIR/lib:$JULIA_DIR/lib/julia" cargo build --features use-bindgen --target x86_64-pc-windows-gnu
 updated=$(sed -e '1h;2,$H;$!d;g' -r -E -e 's/(extern "C" \{\n\s+pub static)/#[link(name = \"libjulia\")]\n\1/g' ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs)
-echo "#![allow(deref_nullptr)]" > ./src/bindings_1_8_x86_64_pc_windows_gnu.rs
-echo "/* generated from Julia version 1.8.0-rc1 */" >> ./src/bindings_1_8_x86_64_pc_windows_gnu.rs
+echo "/* generated from Julia version 1.8.0-rc1 */" > ./src/bindings_1_8_x86_64_pc_windows_gnu.rs
 echo -e "$updated" >> ./src/bindings_1_8_x86_64_pc_windows_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_STABLE_DIR LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$JULIA_DIR/lib:$JULIA_DIR/lib/julia" cargo build --features use-bindgen
-echo "#![allow(deref_nullptr)]" > ./src/bindings_1_8_x86_64_unknown_linux_gnu.rs
-echo "/* generated from Julia version 1.8.0-rc1 */" >> ./src/bindings_1_8_x86_64_unknown_linux_gnu.rs
+echo "/* generated from Julia version 1.8.0-rc1 */" > ./src/bindings_1_8_x86_64_unknown_linux_gnu.rs
 cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_8_x86_64_unknown_linux_gnu.rs
--- jl_sys/src/lib.rs
+++ jl_sys/src/lib.rs
@@ -268,6 +268,10 @@ pub const jl_init: unsafe extern "C" fn() = jl_init__threading;
 pub const jl_init_with_image: unsafe extern "C" fn(*const c_char, *const c_char) =
     jl_init_with_image__threading;
 
+extern "C" {
+    pub fn uv_async_send(async_: *mut c_void) -> ::std::os::raw::c_int;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
