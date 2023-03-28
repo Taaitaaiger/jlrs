3253ada148bbb2237233cd940c26df39a5bad562
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2023-03-28 19:47:22 +0200

    uv_async_send should have raw dylib linkage on Windows, and only needs to be exposed if the uv feature is enabled. Try to fix warnings in BinaryBuilder with the nop-fun-dllimport flag.

--- jl_sys/build.rs
+++ jl_sys/build.rs
@@ -147,11 +147,8 @@ fn compile_jlrs_cc(julia_dir: &str) {
             #[cfg(feature = "windows")]
             c.flag("-mwindows");
 
-            #[cfg(all(feature = "windows", feature = "uv"))]
-            {
-                println!("cargo:rustc-link-search={}/bin", &julia_dir);
-                println!("cargo:rustc-link-lib=uv-2");
-            }
+            #[cfg(feature = "windows")]
+            c.flag("-mnop-fun-dllimport");
         } else {
             #[cfg(feature = "i686")]
             c.flag("-march=pentium4");
