683359eece5795043309aab1b4c02b8a42b9ab0a
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2023-04-07 20:22:30 +0200

    Move bindings into a separate module. Export bindings for macOS. Clean up lib.rs

--- jl_sys/generate_bindings.sh
+++ jl_sys/generate_bindings.sh
@@ -119,11 +119,11 @@ if [ "${NIGHTLY}" = "y" -o "${ALL}" = "y" ]; then
     JULIA_COMMIT=$($JULIA_NIGHTLY_DIR/bin/julia -E "Base.GIT_VERSION_INFO.commit_short" | grep -oEe "[^\"]+")
     JULIA_COMMIT_DATE=$($JULIA_NIGHTLY_DIR/bin/julia -E "Base.GIT_VERSION_INFO.date_string" | grep -oEe "[^\"]+")
     JULIA_DIR=$JULIA_NIGHTLY_DIR cargo build --features use-bindgen,julia-1-10
-    echo "/* generated from $JULIA_VERSION (Commit: $JULIA_COMMIT $JULIA_COMMIT_DATE) */" > ./src/bindings_nightly_x86_64_unknown_linux_gnu.rs
-    cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_nightly_x86_64_unknown_linux_gnu.rs
+    echo "/* generated from $JULIA_VERSION (Commit: $JULIA_COMMIT $JULIA_COMMIT_DATE) */" > ./src/bindings/bindings_nightly_x86_64_unknown_linux_gnu.rs
+    cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_nightly_x86_64_unknown_linux_gnu.rs
 
     if [ "${BETA}" != "y" -a "${ALL}" != "y"  ]; then
-        rustfmt ./src/bindings_*
+        rustfmt ./src/bindings/bindings_*
         exit
     fi
 fi
@@ -156,21 +156,21 @@ if [ "${BETA}" = "y" -o "${ALL}" = "y" ]; then
     cargo clean
     JULIA_VERSION=$($JULIA_1_9_DIR/bin/julia --version)
     JULIA_DIR=$JULIA_1_9_DIR cargo build --features use-bindgen,julia-1-9
-    echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_9_x86_64_unknown_linux_gnu.rs
-    cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_9_x86_64_unknown_linux_gnu.rs
+    echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_9_x86_64_unknown_linux_gnu.rs
+    cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_9_x86_64_unknown_linux_gnu.rs
 
     cargo clean
     JULIA_DIR=$JULIA_1_9_DIR_32 cargo build --features use-bindgen,i686,julia-1-9 --target i686-unknown-linux-gnu
-    echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_9_i686_unknown_linux_gnu.rs
-    cat ../target/i686-unknown-linux-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_9_i686_unknown_linux_gnu.rs
+    echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_9_i686_unknown_linux_gnu.rs
+    cat ../target/i686-unknown-linux-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_9_i686_unknown_linux_gnu.rs
 
     cargo clean
     JULIA_DIR=$JULIA_1_9_DIR_WIN cargo build --features use-bindgen,windows,julia-1-9 --target x86_64-pc-windows-gnu
-    echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_9_x86_64_pc_windows_gnu.rs
-    cat ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_9_x86_64_pc_windows_gnu.rs
+    echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_9_x86_64_pc_windows_gnu.rs
+    cat ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_9_x86_64_pc_windows_gnu.rs
 
     if [ "${ALL}" != "y"  ]; then
-        rustfmt ./src/bindings_*
+        rustfmt ./src/bindings/bindings_*
         exit
     fi
 fi
@@ -250,51 +250,51 @@ fi
 cargo clean
 JULIA_VERSION=$($JULIA_1_6_DIR/bin/julia --version)
 JULIA_DIR=$JULIA_1_6_DIR cargo build --features use-bindgen,julia-1-6
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_6_x86_64_unknown_linux_gnu.rs
-cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_6_x86_64_unknown_linux_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_6_x86_64_unknown_linux_gnu.rs
+cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_6_x86_64_unknown_linux_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_1_6_DIR_32 cargo build --features use-bindgen,julia-1-6,i686 --target i686-unknown-linux-gnu
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_6_i686_unknown_linux_gnu.rs
-cat ../target/i686-unknown-linux-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_6_i686_unknown_linux_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_6_i686_unknown_linux_gnu.rs
+cat ../target/i686-unknown-linux-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_6_i686_unknown_linux_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_1_6_DIR_WIN cargo build --features use-bindgen,windows,julia-1-6 --target x86_64-pc-windows-gnu
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_6_x86_64_pc_windows_gnu.rs
-cat ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_6_x86_64_pc_windows_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_6_x86_64_pc_windows_gnu.rs
+cat ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_6_x86_64_pc_windows_gnu.rs
 
 cargo clean
 JULIA_VERSION=$($JULIA_1_7_DIR/bin/julia --version)
 JULIA_DIR=$JULIA_1_7_DIR cargo build --features use-bindgen,julia-1-7
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_7_x86_64_unknown_linux_gnu.rs
-cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_7_x86_64_unknown_linux_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_7_x86_64_unknown_linux_gnu.rs
+cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_7_x86_64_unknown_linux_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_1_7_DIR_32 cargo build --features use-bindgen,i686,julia-1-7 --target i686-unknown-linux-gnu
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_7_i686_unknown_linux_gnu.rs
-cat ../target/i686-unknown-linux-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_7_i686_unknown_linux_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_7_i686_unknown_linux_gnu.rs
+cat ../target/i686-unknown-linux-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_7_i686_unknown_linux_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_1_7_DIR_WIN cargo build --features use-bindgen,windows,julia-1-7 --target x86_64-pc-windows-gnu
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_7_x86_64_pc_windows_gnu.rs
-cat ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_7_x86_64_pc_windows_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_7_x86_64_pc_windows_gnu.rs
+cat ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_7_x86_64_pc_windows_gnu.rs
 
 cargo clean
 JULIA_VERSION=$($JULIA_1_8_DIR/bin/julia --version)
 JULIA_DIR=$JULIA_1_8_DIR cargo build --features use-bindgen,julia-1-8
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_8_x86_64_unknown_linux_gnu.rs
-cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_8_x86_64_unknown_linux_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_8_x86_64_unknown_linux_gnu.rs
+cat ../target/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_8_x86_64_unknown_linux_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_1_8_DIR_32 cargo build --features use-bindgen,i686,julia-1-8 --target i686-unknown-linux-gnu
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_8_i686_unknown_linux_gnu.rs
-cat ../target/i686-unknown-linux-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_8_i686_unknown_linux_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_8_i686_unknown_linux_gnu.rs
+cat ../target/i686-unknown-linux-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_8_i686_unknown_linux_gnu.rs
 
 cargo clean
 JULIA_DIR=$JULIA_1_8_DIR_WIN cargo build --features use-bindgen,windows,julia-1-8 --target x86_64-pc-windows-gnu
-echo "/* generated from $JULIA_VERSION */" > ./src/bindings_1_8_x86_64_pc_windows_gnu.rs
-cat ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings_1_8_x86_64_pc_windows_gnu.rs
+echo "/* generated from $JULIA_VERSION */" > ./src/bindings/bindings_1_8_x86_64_pc_windows_gnu.rs
+cat ../target/x86_64-pc-windows-gnu/debug/build/jl-sys*/out/bindings.rs >> ./src/bindings/bindings_1_8_x86_64_pc_windows_gnu.rs
 
 rustup default nightly
-rustfmt ./src/bindings_*
+rustfmt ./src/bindings/bindings_*
 rustup default stable
--- jl_sys/src/lib.rs
+++ jl_sys/src/lib.rs
@@ -3,7 +3,6 @@
 #![allow(non_snake_case)]
 
 use cfg_if::cfg_if;
-
 cfg_if! {
     if #[cfg(any(
         all(feature = "julia-1-6", feature = "julia-1-7"),
@@ -26,340 +25,236 @@ cfg_if! {
         feature = "julia-1-10",
     )))] {
         compile_error!("A Julia version feature must be enabled");
+    }
+}
+
+use std::{
+    ffi::c_void,
+    mem::size_of,
+    ptr::{null_mut, NonNull},
+    sync::atomic::{AtomicPtr, Ordering},
+};
+
+#[cfg(feature = "julia-1-6")]
+use ::std::os::raw::c_char;
+
+pub mod bindings;
+pub use bindings::*;
+
+#[cfg(feature = "use-bindgen")]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[inline(always)]
+fn llt_align(x: usize, sz: usize) -> usize {
+    (x + sz - 1) & !(sz - 1)
+}
+
+#[inline(always)]
+pub unsafe fn jl_astaggedvalue(v: *mut jl_value_t) -> *mut jl_taggedvalue_t {
+    let sz = size_of::<jl_taggedvalue_t>() / size_of::<usize>();
+    v.cast::<usize>().sub(sz).cast::<jl_taggedvalue_t>()
+}
+
+#[inline(always)]
+pub unsafe fn jl_typeof(v: *mut jl_value_t) -> *mut jl_value_t {
+    ((*jl_astaggedvalue(v)).__bindgen_anon_1.header as usize & !15usize) as *mut jl_value_t
+}
+
+#[inline(always)]
+pub unsafe fn jl_svec_data(t: *mut jl_svec_t) -> *mut *mut jl_value_t {
+    t.cast::<u8>().add(size_of::<jl_svec_t>()).cast()
+}
+
+#[inline(always)]
+pub unsafe fn jl_array_data(array: *mut jl_value_t) -> *mut c_void {
+    NonNull::new_unchecked(array)
+        .cast::<jl_array_t>()
+        .as_ref()
+        .data
+        .cast()
+}
+
+#[inline(always)]
+pub unsafe fn jl_array_ndims(array: *mut jl_array_t) -> u16 {
+    NonNull::new_unchecked(array).as_ref().flags.ndims()
+}
+
+#[inline(always)]
+pub unsafe fn jl_array_data_owner(a: *mut jl_array_t) -> *mut jl_value_t {
+    a.cast::<u8>()
+        .add(jlrs_array_data_owner_offset(jl_array_ndims(a)) as usize)
+        .cast::<jl_value_t>()
+}
+
+#[inline(always)]
+pub unsafe fn jl_get_fieldtypes(st: *mut jl_datatype_t) -> *mut jl_svec_t {
+    let tys = NonNull::new_unchecked(st).as_ref().types;
+    if tys.is_null() {
+        jl_compute_fieldtypes(st, null_mut())
     } else {
-        use std::{
-            ffi::c_void,
-            mem::size_of,
-            ptr::{null_mut, NonNull},
-            sync::atomic::{AtomicPtr, Ordering},
-        };
+        tys
+    }
+}
 
-        #[cfg(feature = "julia-1-6")]
-        use ::std::os::raw::c_char;
+#[inline(always)]
+pub unsafe fn jl_dt_layout_fields(d: *const u8) -> *const u8 {
+    d.add(size_of::<jl_datatype_layout_t>())
+}
 
-        #[rustfmt::skip]
-        macro_rules! bindings_for {
-            ($bindings:tt, $version:literal, $os:literal, $pointer_width:literal) => {
-                #[cfg(all(
-                    not(feature = "use-bindgen"),
-                    feature = $version,
-                    target_os = $os,
-                    target_pointer_width = $pointer_width
-                ))]
-                mod $bindings;
-                #[cfg(all(
-                    not(feature = "use-bindgen"),
-                    feature = $version,
-                    target_os = $os,
-                    target_pointer_width = $pointer_width
-                ))]
-                pub use $bindings::*;
-            };
+#[inline(always)]
+pub unsafe fn jl_array_ndimwords(ndims: u32) -> i32 {
+    if ndims < 3 {
+        0
+    } else {
+        ndims as i32 - 2
+    }
+}
+
+#[inline(always)]
+pub unsafe fn jl_gc_wb(parent: *mut jl_value_t, ptr: *mut jl_value_t) {
+    let parent_tagged = NonNull::new_unchecked(jl_astaggedvalue(parent)).as_ref();
+    let ptr = NonNull::new_unchecked(jl_astaggedvalue(ptr)).as_ref();
+
+    if parent_tagged.__bindgen_anon_1.bits.gc() == 3 && (ptr.__bindgen_anon_1.bits.gc() & 1) == 0 {
+        jl_gc_queue_root(parent)
+    }
+}
+
+#[inline(always)]
+pub unsafe fn jl_symbol_name_(s: *mut jl_sym_t) -> *mut u8 {
+    s.cast::<u8>()
+        .add(llt_align(size_of::<jl_sym_t>(), size_of::<*mut c_void>()))
+}
+
+#[inline(always)]
+pub unsafe fn jl_fielddesc_size(fielddesc_type: i8) -> u32 {
+    2 << fielddesc_type
+}
+
+#[inline(always)]
+pub unsafe fn jl_field_isptr(st: *mut jl_datatype_t, i: i32) -> bool {
+    let ly = NonNull::new_unchecked(
+        NonNull::new_unchecked(st).as_ref().layout as *mut jl_datatype_layout_t,
+    )
+    .as_ref();
+    assert!(i >= 0 && (i as u32) < ly.nfields);
+    NonNull::new_unchecked(
+        jl_dt_layout_fields(ly as *const _ as *mut u8)
+            .add(jl_fielddesc_size(ly.fielddesc_type() as i8) as usize * i as usize)
+            as *mut jl_fielddesc8_t,
+    )
+    .as_ref()
+    .isptr()
+        != 0
+}
+
+#[inline(always)]
+pub unsafe fn jl_field_size(st: *mut jl_datatype_t, i: isize) -> u32 {
+    let ly = NonNull::new_unchecked(
+        NonNull::new_unchecked(st).as_ref().layout as *mut jl_datatype_layout_t,
+    )
+    .as_ref();
+    assert!(i >= 0 && (i as u32) < ly.nfields);
+    match ly.fielddesc_type() {
+        0 => (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
+            .cast::<jl_fielddesc8_t>()
+            .offset(i)))
+            .size() as u32,
+        1 => (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
+            .cast::<jl_fielddesc16_t>()
+            .offset(i)))
+            .size() as u32,
+        _ => (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
+            .cast::<jl_fielddesc32_t>()
+            .offset(i)))
+            .size(),
+    }
+}
+
+#[inline(always)]
+pub unsafe fn jl_field_offset(st: *mut jl_datatype_t, i: isize) -> u32 {
+    let ly = &*(&*st).layout;
+    assert!(i >= 0 && (i as u32) < ly.nfields);
+    match ly.fielddesc_type() {
+        0 => {
+            (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
+                .cast::<jl_fielddesc8_t>()
+                .offset(i)))
+                .offset as u32
         }
-
-        bindings_for!(
-            bindings_1_6_x86_64_unknown_linux_gnu,
-            "julia-1-6",
-            "linux",
-            "64"
-        );
-        bindings_for!(
-            bindings_1_6_i686_unknown_linux_gnu,
-            "julia-1-6",
-            "linux",
-            "32"
-        );
-        bindings_for!(
-            bindings_1_6_x86_64_pc_windows_gnu,
-            "julia-1-6",
-            "windows",
-            "64"
-        );
-
-        bindings_for!(
-            bindings_1_7_x86_64_unknown_linux_gnu,
-            "julia-1-7",
-            "linux",
-            "64"
-        );
-
-        bindings_for!(
-            bindings_1_7_i686_unknown_linux_gnu,
-            "julia-1-7",
-            "linux",
-            "32"
-        );
-
-        bindings_for!(
-            bindings_1_7_x86_64_pc_windows_gnu,
-            "julia-1-7",
-            "windows",
-            "64"
-        );
-
-        bindings_for!(
-            bindings_1_8_x86_64_unknown_linux_gnu,
-            "julia-1-8",
-            "linux",
-            "64"
-        );
-        bindings_for!(
-            bindings_1_8_i686_unknown_linux_gnu,
-            "julia-1-8",
-            "linux",
-            "32"
-        );
-        bindings_for!(
-            bindings_1_8_x86_64_pc_windows_gnu,
-            "julia-1-8",
-            "windows",
-            "64"
-        );
-
-        bindings_for!(
-            bindings_1_9_x86_64_unknown_linux_gnu,
-            "julia-1-9",
-            "linux",
-            "64"
-        );
-        bindings_for!(
-            bindings_1_9_i686_unknown_linux_gnu,
-            "julia-1-9",
-            "linux",
-            "32"
-        );
-        bindings_for!(
-            bindings_1_9_x86_64_pc_windows_gnu,
-            "julia-1-9",
-            "windows",
-            "64"
-        );
-
-        bindings_for!(
-            bindings_nightly_x86_64_unknown_linux_gnu,
-            "julia-1-10",
-            "linux",
-            "64"
-        );
-
-        #[cfg(feature = "use-bindgen")]
-        include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-        #[inline(always)]
-        fn llt_align(x: usize, sz: usize) -> usize {
-            (x + sz - 1) & !(sz - 1)
+        1 => {
+            (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
+                .cast::<jl_fielddesc16_t>()
+                .offset(i)))
+                .offset as u32
         }
-
-        #[inline(always)]
-        pub unsafe fn jl_astaggedvalue(v: *mut jl_value_t) -> *mut jl_taggedvalue_t {
-            let v_usize = v as usize;
-            let sz = size_of::<jl_taggedvalue_t>();
-
-            (v_usize - sz) as *mut jl_taggedvalue_t
+        _ => {
+            (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
+                .cast::<jl_fielddesc32_t>()
+                .offset(i)))
+                .offset
         }
+    }
+}
 
-        #[inline(always)]
-        pub unsafe fn jl_typeof(v: *mut jl_value_t) -> *mut jl_value_t {
-            ((*jl_astaggedvalue(v)).__bindgen_anon_1.header as usize & !15usize) as *mut jl_value_t
+#[inline(always)]
+pub unsafe fn jl_array_dims_ptr<'a>(array: *mut jl_array_t) -> *mut usize {
+    &mut NonNull::new_unchecked(array).as_mut().nrows
+}
+
+#[inline(always)]
+pub unsafe fn jl_array_ptr_set(a: *mut jl_array_t, i: usize, x: *mut c_void) -> *mut jl_value_t {
+    assert!(NonNull::new_unchecked(a).as_ref().flags.ptrarray() != 0);
+    let a_data: *mut AtomicPtr<jl_value_t> = jl_array_data(a.cast()).cast();
+
+    NonNull::new_unchecked(a_data.add(i))
+        .as_ref()
+        .store(x.cast(), Ordering::Release);
+
+    if !x.is_null() {
+        if NonNull::new_unchecked(a).as_ref().flags.how() == 3 {
+            jl_gc_wb(jl_array_data_owner(a).cast(), x.cast());
+        } else {
+            jl_gc_wb(a.cast(), x.cast());
         }
+    }
 
-        #[inline(always)]
-        pub unsafe fn jl_svec_data(t: *mut jl_svec_t) -> *mut *mut jl_value_t {
-            t.cast::<u8>().add(size_of::<jl_svec_t>()).cast()
-        }
+    x.cast()
+}
 
-        #[inline(always)]
-        pub unsafe fn jl_array_data(array: *mut jl_value_t) -> *mut c_void {
-            NonNull::new_unchecked(array)
-                .cast::<jl_array_t>()
-                .as_ref()
-                .data
-                .cast()
-        }
+#[cfg(feature = "julia-1-6")]
+pub const jl_init: unsafe extern "C" fn() = jl_init__threading;
 
-        #[inline(always)]
-        pub unsafe fn jl_array_ndims(array: *mut jl_array_t) -> u16 {
-            NonNull::new_unchecked(array).as_ref().flags.ndims()
-        }
+#[cfg(feature = "julia-1-6")]
+pub const jl_init_with_image: unsafe extern "C" fn(*const c_char, *const c_char) =
+    jl_init_with_image__threading;
 
-        #[inline(always)]
-        pub unsafe fn jl_array_data_owner(a: *mut jl_array_t) -> *mut jl_value_t {
-            a.cast::<u8>()
-                .add(jlrs_array_data_owner_offset(jl_array_ndims(a)) as usize)
-                .cast::<jl_value_t>()
-        }
+#[cfg(all(feature = "uv", any(feature = "windows", target_os = "windows")))]
+#[link(name = "libuv-2", kind = "raw-dylib")]
+extern "C" {
+    pub fn uv_async_send(async_: *mut c_void) -> ::std::os::raw::c_int;
+}
 
-        #[inline(always)]
-        pub unsafe fn jl_get_fieldtypes(st: *mut jl_datatype_t) -> *mut jl_svec_t {
-            let tys = NonNull::new_unchecked(st).as_ref().types;
-            if tys.is_null() {
-                jl_compute_fieldtypes(st, null_mut())
-            } else {
-                tys
-            }
-        }
+#[cfg(all(feature = "uv", not(any(feature = "windows", target_os = "windows"))))]
+extern "C" {
+    pub fn uv_async_send(async_: *mut c_void) -> ::std::os::raw::c_int;
+}
 
-        #[inline(always)]
-        pub unsafe fn jl_dt_layout_fields(d: *const u8) -> *const u8 {
-            d.add(size_of::<jl_datatype_layout_t>())
-        }
+#[cfg(test)]
+mod tests {
+    use std::ffi::CString;
 
-        #[inline(always)]
-        pub unsafe fn jl_array_ndimwords(ndims: u32) -> i32 {
-            if ndims < 3 {
-                0
-            } else {
-                ndims as i32 - 2
-            }
-        }
+    use super::*;
 
-        #[inline(always)]
-        pub unsafe fn jl_gc_wb(parent: *mut jl_value_t, ptr: *mut jl_value_t) {
-            let parent_tagged = NonNull::new_unchecked(jl_astaggedvalue(parent)).as_ref();
-            let ptr = NonNull::new_unchecked(jl_astaggedvalue(ptr)).as_ref();
-
-            if parent_tagged.__bindgen_anon_1.bits.gc() == 3 && (ptr.__bindgen_anon_1.bits.gc() & 1) == 0 {
-                jl_gc_queue_root(parent)
-            }
-        }
-
-        #[inline(always)]
-        pub unsafe fn jl_symbol_name_(s: *mut jl_sym_t) -> *mut u8 {
-            s.cast::<u8>()
-                .add(llt_align(size_of::<jl_sym_t>(), size_of::<*mut c_void>()))
-        }
-
-        #[inline(always)]
-        pub unsafe fn jl_fielddesc_size(fielddesc_type: i8) -> u32 {
-            2 << fielddesc_type
-        }
-
-        #[inline(always)]
-        pub unsafe fn jl_field_isptr(st: *mut jl_datatype_t, i: i32) -> bool {
-            let ly = NonNull::new_unchecked(
-                NonNull::new_unchecked(st).as_ref().layout as *mut jl_datatype_layout_t,
-            )
-            .as_ref();
-            assert!(i >= 0 && (i as u32) < ly.nfields);
-            NonNull::new_unchecked(
-                jl_dt_layout_fields(ly as *const _ as *mut u8)
-                    .add(jl_fielddesc_size(ly.fielddesc_type() as i8) as usize * i as usize)
-                    as *mut jl_fielddesc8_t,
-            )
-            .as_ref()
-            .isptr()
-                != 0
-        }
-
-        #[inline(always)]
-        pub unsafe fn jl_field_size(st: *mut jl_datatype_t, i: isize) -> u32 {
-            let ly = NonNull::new_unchecked(
-                NonNull::new_unchecked(st).as_ref().layout as *mut jl_datatype_layout_t,
-            )
-            .as_ref();
-            assert!(i >= 0 && (i as u32) < ly.nfields);
-            match ly.fielddesc_type() {
-                0 => (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
-                    .cast::<jl_fielddesc8_t>()
-                    .offset(i)))
-                    .size() as u32,
-                1 => (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
-                    .cast::<jl_fielddesc16_t>()
-                    .offset(i)))
-                    .size() as u32,
-                _ => (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
-                    .cast::<jl_fielddesc32_t>()
-                    .offset(i)))
-                    .size(),
-            }
-        }
-
-        #[inline(always)]
-        pub unsafe fn jl_field_offset(st: *mut jl_datatype_t, i: isize) -> u32 {
-            let ly = &*(&*st).layout;
-            assert!(i >= 0 && (i as u32) < ly.nfields);
-            match ly.fielddesc_type() {
-                0 => {
-                    (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
-                        .cast::<jl_fielddesc8_t>()
-                        .offset(i)))
-                        .offset as u32
-                }
-                1 => {
-                    (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
-                        .cast::<jl_fielddesc16_t>()
-                        .offset(i)))
-                        .offset as u32
-                }
-                _ => {
-                    (&*(jl_dt_layout_fields((ly as *const jl_datatype_layout_t).cast())
-                        .cast::<jl_fielddesc32_t>()
-                        .offset(i)))
-                        .offset
-                }
-            }
-        }
-
-        #[inline(always)]
-        pub unsafe fn jl_array_dims_ptr<'a>(array: *mut jl_array_t) -> *mut usize {
-            &mut NonNull::new_unchecked(array).as_mut().nrows
-        }
-
-        #[inline(always)]
-        pub unsafe fn jl_array_ptr_set(a: *mut jl_array_t, i: usize, x: *mut c_void) -> *mut jl_value_t {
-            assert!(NonNull::new_unchecked(a).as_ref().flags.ptrarray() != 0);
-            let a_data: *mut AtomicPtr<jl_value_t> = jl_array_data(a.cast()).cast();
-
-            NonNull::new_unchecked(a_data.add(i))
-                .as_ref()
-                .store(x.cast(), Ordering::Release);
-
-            if !x.is_null() {
-                if NonNull::new_unchecked(a).as_ref().flags.how() == 3 {
-                    jl_gc_wb(jl_array_data_owner(a).cast(), x.cast());
-                } else {
-                    jl_gc_wb(a.cast(), x.cast());
-                }
-            }
-
-            x.cast()
-        }
-
-        #[cfg(feature = "julia-1-6")]
-        pub const jl_init: unsafe extern "C" fn() = jl_init__threading;
-
-        #[cfg(feature = "julia-1-6")]
-        pub const jl_init_with_image: unsafe extern "C" fn(*const c_char, *const c_char) =
-            jl_init_with_image__threading;
-
-        #[cfg(all(feature = "uv", any(feature = "windows", target_os = "windows")))]
-        #[link(name = "libuv-2", kind = "raw-dylib")]
-        extern "C" {
-            pub fn uv_async_send(async_: *mut c_void) -> ::std::os::raw::c_int;
-        }
-
-        #[cfg(all(feature = "uv", not(any(feature = "windows", target_os = "windows"))))]
-        extern "C" {
-            pub fn uv_async_send(async_: *mut c_void) -> ::std::os::raw::c_int;
-        }
-
-        #[cfg(test)]
-        mod tests {
-            use std::ffi::CString;
-
-            use super::*;
-
-            #[test]
-            fn sanity() {
-                unsafe {
-                    jl_init();
-                    assert!(jl_is_initialized() != 0);
-                    let cmd = CString::new("sqrt(2.0)").unwrap();
-                    jl_eval_string(cmd.as_ptr());
-                    assert!(jl_exception_occurred().is_null());
-                    jl_atexit_hook(0);
-                }
-            }
+    #[test]
+    fn sanity() {
+        unsafe {
+            jl_init();
+            assert!(jl_is_initialized() != 0);
+            let cmd = CString::new("sqrt(2.0)").unwrap();
+            jl_eval_string(cmd.as_ptr());
+            assert!(jl_exception_occurred().is_null());
+            jl_atexit_hook(0);
         }
     }
 }
