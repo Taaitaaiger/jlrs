fbb39567941baf4b253c90565bd9f019115c18b7
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2023-04-10 19:05:57 +0200

    Fix a few linking errors

--- jl_sys/src/lib.rs
+++ jl_sys/src/lib.rs
@@ -229,8 +229,15 @@ pub const jl_init: unsafe extern "C" fn() = jl_init__threading;
 pub const jl_init_with_image: unsafe extern "C" fn(*const c_char, *const c_char) =
     jl_init_with_image__threading;
 
+#[cfg(not(target_pointer_width = "32"))]
 #[cfg(all(feature = "uv", any(feature = "windows", target_os = "windows")))]
 #[link(name = "libuv-2", kind = "raw-dylib")]
+extern "C" {
+    pub fn uv_async_send(async_: *mut c_void) -> ::std::os::raw::c_int;
+}
+
+#[cfg(target_pointer_width = "32")]
+#[cfg(all(feature = "uv", any(feature = "windows", target_os = "windows")))]
 extern "C" {
     pub fn uv_async_send(async_: *mut c_void) -> ::std::os::raw::c_int;
 }
