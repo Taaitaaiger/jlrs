62c38e6ae0e96b992a33a1218f2df69892b2a47e
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2024-07-10 20:05:10 +0200

    Move PrimitiveType to its own module in data::types

--- jlrs/src/data/layout/julia_enum.rs
+++ jlrs/src/data/layout/julia_enum.rs
@@ -1,72 +1,23 @@
-#![allow(unused)]
-
-use std::mem::size_of;
+//! Convert a Rust enum to a Julia enum.
 
 use crate::{
-    data::types::{
-        abstract_type::{AbstractChar, AbstractFloat, Integer, Signed, Unsigned},
-        construct_type::ConstructType,
-    },
-    inline_static_ref,
+    data::types::primitive_type::IntegerType,
     prelude::{Target, Value},
 };
 
-pub trait PrimitiveType: Copy {
-    const N_BITS: usize;
-    type Super: ConstructType;
-}
-
-pub trait IntegerType: PrimitiveType {}
-
-macro_rules! impl_primitive_type {
-    ($ty:ty, $super:ty) => {
-        impl PrimitiveType for $ty {
-            const N_BITS: usize = size_of::<Self>();
-            type Super = $super;
-        }
-    };
-}
-
-impl_primitive_type!(u8, Unsigned);
-impl_primitive_type!(u16, Unsigned);
-impl_primitive_type!(u32, Unsigned);
-impl_primitive_type!(u64, Unsigned);
-impl_primitive_type!(usize, Unsigned);
-impl_primitive_type!(i8, Signed);
-impl_primitive_type!(i16, Signed);
-impl_primitive_type!(i32, Signed);
-impl_primitive_type!(i64, Signed);
-impl_primitive_type!(isize, Signed);
-
-impl IntegerType for u8 {}
-impl IntegerType for u16 {}
-impl IntegerType for u32 {}
-impl IntegerType for u64 {}
-impl IntegerType for usize {}
-impl IntegerType for i8 {}
-impl IntegerType for i16 {}
-impl IntegerType for i32 {}
-impl IntegerType for i64 {}
-impl IntegerType for isize {}
-
-impl_primitive_type!(bool, Integer);
-impl_primitive_type!(super::bool::Bool, Integer);
-
-impl IntegerType for bool {}
-impl IntegerType for char {}
-
-impl_primitive_type!(char, AbstractChar);
-impl_primitive_type!(super::char::Char, AbstractChar);
-
-impl IntegerType for super::bool::Bool {}
-impl IntegerType for super::char::Char {}
-
-// impl_primitive_type!(f16, AbstractFloat);
-impl_primitive_type!(f32, AbstractFloat);
-impl_primitive_type!(f64, AbstractFloat);
-
+/// Trait to convert a Rust enum to a Julia enum.
+///
+/// This trait is automatically derived when bindings for enums are generated with
+/// `JlrsCore.Reflect.reflect`.
+///
+/// Safety: the implementation must correctly map the Rust to the Julia enum.
 pub unsafe trait Enum {
+    /// The type that represents this enum.
     type Super: IntegerType;
+
+    /// Convert `self` to a `Value`
     fn as_value<'target, Tgt: Target<'target>>(&self, _: &Tgt) -> Value<'target, 'static>;
+
+    /// Convert `self` to its representation
     fn as_super(&self) -> Self::Super;
 }
