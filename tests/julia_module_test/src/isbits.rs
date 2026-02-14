cde35fd39a974b64d3c80bfbb3ae1b4552948a46
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2026-02-14 17:47:30 +0100

    Add support for u128 and i128

--- tests/julia_module_test/src/isbits.rs
+++ tests/julia_module_test/src/isbits.rs
@@ -7,3 +7,11 @@ pub fn takes_no_args_returns_usize() -> usize {
 pub fn takes_usize_returns_usize(a: usize) -> usize {
     a + 1
 }
+
+pub fn u128_sum(a: u128, b: u128) -> u128 {
+    a + b
+}
+
+pub fn i128_sum(a: i128, b: i128) -> i128 {
+    a + b
+}
