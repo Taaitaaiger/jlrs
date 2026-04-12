63026fd553996eb4d2d5a083aa596d6f6eb04584
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2026-04-12 17:38:09 +0200

    Reenable disabled tests

--- tests/script/JuliaModule.jl
+++ tests/script/JuliaModule.jl
@@ -16,9 +16,9 @@ mutable struct FourGenericsM{A, B, C, D}
 end 
 
 separator = Sys.iswindows() ? '\\' : '/'
-test_library_path = get(ENV, "JULIA_MODULE_TEST_LIB_DIR", ".$separator")
+test_library_dir = get(ENV, "JULIA_MODULE_TEST_LIB_DIR", ".")
 test_library_name = Sys.iswindows() ? "julia_module_test" : "libjulia_module_test"
-@wrapmodule("$(test_library_path)$(test_library_name)", :julia_module_tests_init_fn)
+@wrapmodule("$(test_library_dir)$(separator)$(test_library_name)", :julia_module_tests_init_fn)
 
 function __init__()
     @initjlrs
