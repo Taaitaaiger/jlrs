78a127f5a8a1073ea6bc9d267565571ba37dc3ae
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2026-04-17 18:33:08 +0200

    Fix coverage script

--- coverage.sh
+++ coverage.sh
@@ -2,20 +2,14 @@
 
 set -euxo pipefail
 
-#export JLRS_JULIA_DIR=$HOME/julia-1.9.3
-#export LD_LIBRARY_PATH=$JLRS_JULIA_DIR/lib:$JLRS_JULIA_DIR/lib/julia
-echo "backend: Gtk3Agg" > matplotlibrc
-
 cargo llvm-cov clean --workspace;
 cargo llvm-cov --features full,ccall --workspace --no-report
 cargo llvm-cov --example ccall --no-report -- --test-threads=1
+cargo llvm-cov --example ccall_throw_exception --no-report -- --test-threads=1
 cargo llvm-cov run --example async_tasks --no-report
 cargo llvm-cov run --example call_julia --no-report
-cargo llvm-cov run --example fully_async_async_std --no-report
-cargo llvm-cov run --example fully_async_tokio --no-report
 cargo llvm-cov run --example nested_async_scopes --no-report
 cargo llvm-cov run --example persistent_tasks --no-report
-cargo llvm-cov run --example plot --no-report
-rm matplotlibrc
+cargo llvm-cov run --example with_rayon --no-report
 
-cargo llvm-cov --no-run --open --hide-instantiations --ignore-filename-regex "(managed/internal|jl_sys)"
+cargo llvm-cov --no-run --open --hide-instantiations --ignore-filename-regex "(jl_sys|jlrs_sys|build_utils)"
